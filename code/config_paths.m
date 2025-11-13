91939295dd3be2b4be0194be75ed474310ab379b
Dryden Arseneau <darsenea@uwo.ca>
2025-11-13 15:45:14 -0500

    Edited preprocessing

--- code/config_paths.m
+++ code/config_paths.m
@@ -2,18 +2,18 @@
 function P = config_paths()
 
 % ---- Root ----
-P.PROJ_ROOT = fullfile('/Users','drydena18','Desktop','masters_thesis');
+P.PROJ_ROOT = fullfile('/cifs','seminowicz','eegPainDatasets','CNED');
 
 % ---- BIDS dataset root ----
-P.BIDS = fullfile(P.PROJ_ROOT, 'data', 'BIDS_26_ByBiosemi');
+P.BIDS = fullfile(P.PROJ_ROOT, '26ByBiosemi');
 
 % ---- Core Inputs ----
 P.PARTICIPANTS_TSV = fullfile(P.BIDS, 'participants.tsv');
 P.DATASET_EVENTS_JSON = fullfile(P.BIDS, 'task-26ByBiosemi_events.json');
-P.CSV_SINGLETRIAL = fullfile(P.PROJ_ROOT, 'data', 'CSV', 'participants_singletrial_26ByBiosemi.csv');
+P.CSV_SINGLETRIAL = fullfile(P.PROJ_ROOT, 'data', 'CSV', 'participants_singletrial_26ByBiosemi.csv'); %% Need to downlaod
 
 % ---- Derivatives ----
-P.DERIV = fullfile(P.BIDS, 'derivatives');
+P.DERIV = fullfile(P.PROJ_ROOT,'da-analysis','26ByBiosemi','derivatives');
 P.SESSION_MERGED = fullfile(P.DERIV, 'session_merged_data');
 P.FILTER = fullfile(P.DERIV, 'filter');
 P.NO_ICA = fullfile(P.DERIV, 'no_ica');
@@ -23,7 +23,7 @@ P.REFILTER_30 = fullfile(P.DERIV, 'refilter_30');
 P.REREFER = fullfile(P.DERIV, 'rerefer');
 
 % ---- Resources ----
-P.ELP_FILE = fullfile(P.PROJ_ROOT, 'resources', 'standard-10-5-cap385.elp');
+P.ELP_FILE = fullfile(P.PROJ_ROOT, 'resources', 'standard-10-5-cap385.elp'); %% Need to download
 
 % Ensure derivative folders exist ----
 outDirs = {P.SESSION_MERGED, P.FILTER, P.NO_ICA, P.ICA, P.AFTER_ICA, P.REFILTER_30, P.REREFER};
