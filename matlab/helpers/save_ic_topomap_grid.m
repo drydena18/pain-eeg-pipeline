e572b5571c689ec45791328e0dd9b1e9328750fb
Dryden Arseneau <darsenea@uwo.ca>
2026-09-08 15:04:19 -0400

    change helper name

--- matlab/helpers/save_ic_topomap_grid.m
+++ matlab/helpers/save_ic_topomap_grid.m
@@ -1,4 +1,4 @@
-function save_ic_topo_grid(QC, subjid, EEG, nICs)
+function save_ic_topomap_grid(QC, subjid, EEG, nICs)
 % SAVE_IC_TOPO_GRID Save one figure with topomaps for the first N ICs.
 %   Complements save_ic_qc_packets.m, which only generates full 4-panel
 %   figures for ICLabel-flagged ICs. This gives a quick-look grid across
